2b3666bfe1c0c6e582d1c648784d3c0f00b8d88c
Sliim <sliim@mailoo.org>
2015-03-25 19:50:11 +0100

    Bump version 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ kali CHANGELOG
 
 This file is used to list changes made in each version of the kali cookbook.
 
+0.1.1
+-----
+- Packages authentication with gpg signature
+
 0.1.0
 -----
 - Initial release of kali cookbook
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sliim@mailoo.org'
 license 'Apache 2.0'
 description 'Installs/Configures kali linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 recipe 'kali::all', 'Install all available packages in Kali linux'
 recipe 'kali::default', 'Setup debian with kali linux official repository'
