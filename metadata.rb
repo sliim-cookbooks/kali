4162db38c36a52a74a23065241b9af9e29b51ae9
Sliim <sliim@mailoo.org>
2017-05-13 12:23:55 +0200

    Update ruby version + fix foodcritic

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 name 'kali'
 maintainer 'Sliim'
 maintainer_email 'sliim@mailoo.org'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures Kali linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+chef_version '>= 12.5' if respond_to?(:chef_version)
 version '1.0.1'
 
 recipe 'kali::all', 'Install all available packages in Kali linux'
